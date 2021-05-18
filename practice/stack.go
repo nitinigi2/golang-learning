f6213cc2c57fd1f1c674d00f6adb9251d74d4d6e
Nitin Singhal <nitinsinghal139@gmail.com>
2021-05-18 15:41:07 +0530

    Update stack class to handle any data type

--- practice/stack.go
+++ practice/stack.go
@@ -35,3 +35,60 @@ func main() {
 		fmt.Println(data)
 	}
 }
+
+// Using interface as type
+// using panic in case of type mismatch
+/*
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Stack []interface{}
+
+func (stack *Stack) Push(ele interface{}) bool {
+	if len(*stack) != 0 {
+		lastInserted := (*stack)[len(*stack)-1]
+
+		if reflect.TypeOf(lastInserted) != reflect.TypeOf(ele) {
+			panic("Type mismatch")
+		}
+	}
+
+	*stack = append([]interface{}{ele}, *stack...)
+
+	return true
+}
+
+func (stack *Stack) IsEmpty() bool {
+	return len(*stack) == 0
+}
+
+func (stack *Stack) Pop() interface{} {
+	old := *stack
+	popped := old[0]
+	if stack.IsEmpty() {
+		panic("stack is empty")
+	}
+	*stack = old[1:]
+	return popped
+}
+
+func main() {
+	stack := Stack{}
+	stack.Push(10)
+	stack.Push(20)
+	stack.Push(30)
+	stack.Push(40)
+	stack.Push(50)
+	stack.Push("abc")
+
+	for !stack.IsEmpty() {
+		data := stack.Pop()
+		fmt.Println(data)
+	}
+}
+
+*/
