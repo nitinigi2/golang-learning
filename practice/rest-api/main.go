5eaad9cde9ff2fa890383d3b4c659e7ac6c72b41
Nitin Singhal <nitinsinghal139@gmail.com>
2021-06-07 11:11:29 +0530

    Add server config file to store server configurations

--- practice/rest-api/main.go
+++ practice/rest-api/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
+
+type Server struct {
+	Ip   string `json:"ip"`
+	Port string `json:"port"`
+}
 
 func main() {
 	r := mux.NewRouter()
 
 	RegisterHandlers(r)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	server := readCofigFile()
+	serverIp := server.Ip
+	serverPort := server.Port
 
+	log.Fatal(http.ListenAndServe(serverIp+":"+serverPort, r))
+
+}
+
+func readCofigFile() *Server {
+	var server Server
+	jsonFile, err := os.Open("serverconfig.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal([]byte(byteValue), &server)
+
+	return &server
 }
